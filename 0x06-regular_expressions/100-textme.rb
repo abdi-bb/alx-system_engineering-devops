a22358fd30b5517bad3c4aa9b3603395cd9034ed
Abdi Berhe <abdi201380@gmail.com>
2023-05-03 08:45:59 -0700

    find and print sender,receiver,flags

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,9 +1,11 @@
 #!/usr/bin/env ruby
 
-input_str = ARGV[0]
+input_string = ARGV[0]
 
-sender = input_str.match(/\[from:([^[\]]+)\]/)&.captures&.first
-receiver = input_str.match(/\[to:([^[\]]+)\]/)&.captures&.first
-flags = input_str.match(/\[flags:([^[\]]+)\]/)&.captures&.first
+sender = input_string.scan(/\[from:([^\]]+)\]/).flatten.first
+
+receiver = input_string.scan(/\[to:([^\]]+)\]/).flatten.first
+
+flags = input_string.scan(/\[flags:([^\]]+)\]/).flatten.first
 
 puts "#{sender},#{receiver},#{flags}"
