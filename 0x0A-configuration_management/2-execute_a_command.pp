d3696fcd290e79d1aa005216e8e5d8b7342e642c
Abdi Berhe <abdi201380@gmail.com>
2023-06-23 09:00:59 -0700

    [Add]: Puppet manifest to kill a process

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -2,6 +2,6 @@
 
 exec { 'killmenow':
   command     => 'pkill -9 killmenow',
-  path        => '/usr/bin:/usr/sbin:/bin:sbin',
+  path        => '/usr/bin:/usr/sbin:/bin:/sbin',
   refreshonly => true,
 }
