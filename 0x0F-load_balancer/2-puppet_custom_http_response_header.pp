f34e36e5e198970ae006577d4e7a1660f458c0df
Abdi Berhe <abdi201380@gmail.com>
2023-07-03 19:02:30 -0700

    [Puppet]: Puppet automation

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,16 +1,20 @@
 # Use Puppet to automate the task of creating a custom HTTP header response
 
-exec {'update':
+exec { 'update':
   command => '/usr/bin/apt-get update',
 }
--> package {'nginx':
+
+package { 'nginx':
   ensure => 'present',
 }
--> file_line { 'http_header':
+
+file_line { 'http_header':
   path  => '/etc/nginx/nginx.conf',
+  line  => 'add_header X-Served-By $hostname;',
   match => 'http {',
-  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
--> exec {'run':
+
+exec { 'run':
   command => '/usr/sbin/service nginx restart',
-}
+  require => [Package['nginx'], File_line['http_header']],
+}
