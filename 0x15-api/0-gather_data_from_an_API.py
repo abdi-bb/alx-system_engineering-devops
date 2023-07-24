9ea9ca7879083ac13de111ef40e73f6b79aeee0d
Abdi Berhe <abdi201380@gmail.com>
2023-07-24 14:21:43 -0700

    [Script]: That returns information of an employee from a url

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,7 +6,7 @@ import sys
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user_id = sys.argv[1]
-    user = requests.get(url + "users/{}".format(user_id).json()
+    user = requests.get(url + "users/{}".format(user_id)).json()
     todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
     completed = [task.get("title") for task in todos if task.get("completed")]
