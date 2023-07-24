4a71106703a84956b710ea9c8a332cbd5fe2c18f
Abdi Berhe <abdi201380@gmail.com>
2023-07-24 14:55:49 -0700

    [Script]: Exports to dict format

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Exports to-do list information of all employees to JSON format."""
+"""Exports the previously returned to-do list information to JSON format."""
 import json
 import requests
 
@@ -9,10 +9,10 @@ if __name__ == "__main__":
 
     with open("todo_all_employees.json", "w") as jsonfile:
         json.dump({
-            user.get("id"): [{
+            u.get("id"): [{
                 "task": task.get("title"),
                 "completed": task.get("completed"),
-                "username": user.get("username")
+                "username": u.get("username")
             } for task in requests.get(url + "todos",
-                                    params={"userId": user.get("id")}).json()]
-            for user in users}, jsonfile)
+                                       params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
